a2ec11e554a2fa6f0a6e1c4fda514a2ffd4edd23
Valentin Brosseau <c4software@gmail.com>
2022-05-22 22:17:30 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Timers from './components/Timers.vue';
+
+const etudiants = ref([] as string[]);
+</script>
+
+<template>
+  <div class="text-center">
+    <img alt="BTS SIO" class="logo mx-auto m-10" src="./assets/logo-certa.png" />
+  </div>
+
+  <table class="border-collapse table-auto w-full text-sm">
+    <thead>
+      <tr>
+        <th>Étudiant</th>
+        <th>Préparation</th>
+        <th>Réalisation</th>
+        <th>Présentation</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <Timers v-for="t,c in etudiants" :etudiant="`Étudiant ${c + 1}`" />
+    </tbody>
+  </table>
+
+  <div class="text-center p-10">
+    <button type="button" @click="etudiants.push('')" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Ajouter un étudiant</button>
+  </div>
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo{
+  height: 100px;
+}
+</style>
---
+++ src/components/Timers.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { ref, computed, watch } from 'vue'
+defineProps<{ etudiant: string }>()
+
+let step = ref("preparation")
+let running = ref(false)
+const timer = ref({
+    "preparation": 1800,
+    "realisation": 3600,
+    "presentation": 1800,
+})
+
+const hasRemainingTime = computed(() => timer.value[step.value] > 0)
+
+const buttonName = computed(() => {
+    if(hasRemainingTime){
+        return running.value ? 'Stop' : 'Demarrer'
+    } else {
+        return 'Suivant'
+    }
+})
+
+const btnClass = computed(() => running.value ? 'fixed-width px-3 py-2 text-xs font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800' : 'fixed-width px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800')
+
+function toggleTimer(){
+    running.value = !running.value
+}
+
+const timeInterval = ref();
+watch(running, (value) => {
+    if(!value){
+        clearInterval(timeInterval.value)
+    } else {
+        timeInterval.value = setInterval(() => {
+            timer.value[step.value]--;
+        }, 1000)
+    }
+})
+
+function fancyTimeFormat(s: number)
+{   
+    if(s < 0){
+        return s;
+    }
+
+    return(s-(s%=60))/60+(9<s?':':':0')+s
+
+}
+
+</script>
+
+<template>
+
+    <tr>
+        <td>{{etudiant}}</td>
+        <td @click="step = 'preparation'" :class="step === 'preparation' ? 'font-bold' : 'pointer'"><div class="text-xl">{{fancyTimeFormat(timer.preparation)}}</div></td>
+        <td @click="step = 'realisation'" :class="step === 'realisation' ? 'font-bold' : 'pointer'"><div class="text-xl">{{fancyTimeFormat(timer.realisation)}}</div></td>
+        <td @click="step = 'presentation'" :class="step === 'presentation' ? 'font-bold' : 'pointer'"><div class="text-xl">{{fancyTimeFormat(timer.presentation)}}</div></td>
+        <td>
+            <button @click="toggleTimer" type="button" :class="btnClass">{{buttonName}}</button>
+        </td>
+    </tr>
+
+</template>
+
+<style scoped>
+
+.fixed-width {
+    width: 100px;
+}
+
+.pointer{
+    cursor: pointer;
+}
+
+</style>
