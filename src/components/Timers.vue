66e5aa6e410ed6810f1fe651b81ff18f03c1a80d
Valentin Brosseau <c4software@gmail.com>
2022-05-22 22:26:39 +0200

    Retrait du mode suivant

--- src/components/Timers.vue
+++ src/components/Timers.vue
@@ -13,11 +13,7 @@ const timer: any = ref({
 const hasRemainingTime = computed(() => timer.value[step.value] > 0)
 
 const buttonName = computed(() => {
-    if(hasRemainingTime){
-        return running.value ? 'Stop' : 'Demarrer'
-    } else {
-        return 'Suivant'
-    }
+    return running.value ? 'Stop' : 'Demarrer'
 })
 
 const btnClass = computed(() => running.value ? 'fixed-width px-3 py-2 text-xs font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800' : 'fixed-width px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800')
