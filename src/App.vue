cf6918e940aad9fa3dfe758404745870fc2bcb66
Erpriex <erpriex@gmail.com>
2022-05-26 00:50:50 +0200

    Add student name

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, useCssModule } from 'vue'
 import Timers from './components/Timers.vue';
 
 const etudiants = ref([] as string[]);
+
+var studentNameInput = "";
 </script>
 
 <template>
@@ -13,12 +15,13 @@ const etudiants = ref([] as string[]);
   <div class="py-5">
     <main class="h-full overflow-y-auto">
         <div class="container  mx-auto grid">
-          <Timers v-for="t,c in etudiants" :etudiant="`Étudiant ${c + 1}`" />
+          <Timers v-for="t,c in etudiants" :etudiant="`${(t != '' ? t : 'Étudiant ' + (c + 1))}`" />
         </div>
     </main>
 
     <div class="text-center p-10">
-      <button type="button" @click="etudiants.push('')" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Ajouter un étudiant</button>
+      <input class="pl-2 border rounded py-2 px-3" style="margin-right: 2%" type="text" placeholder="Nom étudiant (falcutatif)" v-model="studentNameInput"/>
+      <button type="button" @click="etudiants.push(`${studentNameInput}`); studentNameInput = '';" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Ajouter un étudiant</button>
     </div>
   </div>
 
