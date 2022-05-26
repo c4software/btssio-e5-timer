1bf2b57c1868e15b012fc9960557b33dddcb08fa
Brosseau Valentin <c4software@gmail.com>
2022-05-26 12:08:13 +0200

    Add events & correctly handle student name

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,12 @@ import Timers from './components/Timers.vue';
 
 const etudiants = ref([] as string[]);
 
-var studentNameInput = "";
+let studentNameInput = ref("");
+
+function addStudent(){
+  etudiants.push(studentNameInput); 
+  studentNameInput.value = '';
+}
 </script>
 
 <template>
@@ -19,10 +24,10 @@ var studentNameInput = "";
         </div>
     </main>
 
-    <div class="text-center p-10">
-      <input class="pl-2 border rounded py-2 px-3" style="margin-right: 10px; margin-bottom: 10px;" type="text" placeholder="Nom étudiant (falcutatif)" v-model="studentNameInput"/>
-      <button type="button" @click="etudiants.push(`${studentNameInput}`); studentNameInput = '';" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Ajouter un étudiant</button>
-    </div>
+    <form @submit.prevent="addStudent" class="text-center p-10">
+      <input @keyup.enter="addStudent" class="pl-2 border rounded py-2 px-3" style="margin-right: 10px; margin-bottom: 10px;" type="text" placeholder="Nom étudiant (falcutatif)" v-model="studentNameInput"/>
+      <button type="button" @click="addStudent" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Ajouter un étudiant</button>
+    </form>
   </div>
 
 </template>
