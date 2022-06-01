44cb9ebe3c729644244fe77445f7a23a5f002c64
Valentin Brosseau <c4software@gmail.com>
2022-06-01 22:47:17 +0200

    Disable filename hash

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
